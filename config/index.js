6623c31cab21011f96041b28685a06f3e61efb08
chenhemei <1914163984@qq.com>
2019-01-16 16:57:17 +0800

    改变host的职位localhost

--- config/index.js
+++ config/index.js
@@ -22,8 +22,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    // host: 'localhost', // can be overwritten by process.env.HOST
-    host:'192.168.1.44',
+    host: 'localhost', // can be overwritten by process.env.HOST
+    // host:'192.168.1.44',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
